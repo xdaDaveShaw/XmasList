40e627289570e90b745169b15c9c7c9927f6efc6
Dave Shaw <dave@taeguk.co.uk>
2018-11-04 22:14:17 +0000

    Start prompting for items

--- src/Home/View.fs
+++ src/Home/View.fs
@@ -11,20 +11,33 @@ open Types
 let root model dispatch =
   div [ ] [
     form [ ] [
-      Field.div [ ] [
-        Control.div [ ] [
-          Input.text [
-            Input.Placeholder "Enter your name"
-            Input.Props [ Props.AutoFocus true ]
-            Input.OnChange (fun ev -> !!ev.target?value |> ChangeStr |> dispatch)
-            Input.DefaultValue model.Name
+      if (not model.Accepted) then
+        yield Field.div [ ] [
+          Control.div [ ] [
+            Input.text [
+              Input.Placeholder "Enter your name"
+              Input.Props [ Props.AutoFocus true ]
+              Input.OnChange (fun ev -> !!ev.target?value |> ChangeStr |> dispatch)
+              Input.IsReadOnly model.Accepted
+              Input.DefaultValue model.Name
+            ]
           ]
         ]
-      ]
-      Field.div [ Field.IsGrouped ]
-        [ Control.div [ ] [
-          Button.Input.submit
-            [ Button.Color IsPrimary
-              Button.OnClick (fun _ -> dispatch AcceptName)
-              Button.Props [ Value "Done" ] ]
-            ] ] ] ]
+        yield Field.div [ Field.IsGrouped ]
+          [ Control.div [ ] [
+            Button.Input.submit
+              [ Button.Color IsPrimary
+                Button.OnClick (fun _ -> dispatch AcceptName)
+                Button.Props [ Value "Done" ] ] ] ]
+      else
+        yield Field.div [ ] [
+              Label.label [ Label.Size Size.IsLarge ] [ str ("Welcome " + model.Name) ]
+              Label.label [ ] [ str "Start adding to your Xmas List" ] ]
+        yield Field.div [ Field.HasAddons ] [
+              Control.div [ Control.IsExpanded ] [
+                Input.text [ Input.Placeholder "What do you want?" ]
+              ]
+              Control.div [ ] [
+                Button.button [] [ str "Add"]
+              ] ]
+    ] ]
