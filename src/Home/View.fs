5e4e339329c1a31f1ac8d389de24088eae012f44
Dave Shaw <dave@taeguk.co.uk>
2018-11-07 23:38:26 +0000

    refactoring UI

--- src/Home/View.fs
+++ src/Home/View.fs
@@ -8,49 +8,59 @@ open Fulma
 open Fulma.FontAwesome
 open Types
 
-let unknownUi model dispatch = seq {
-  yield Field.div [ ] [
-    Control.div [ ] [
-      Input.text [
-        Input.Placeholder "Enter your name"
-        Input.Props [ Props.AutoFocus true ]
-        Input.OnChange (fun ev -> !!ev.target?value |> UpdatingName |> dispatch)
-        Input.DefaultValue model.Name
+let unknownUi model dispatch =
+  [
+    Field.div [ ] [
+      Control.div [ ] [
+        Input.text [
+          Input.Placeholder "Enter your name"
+          Input.Props [ Props.AutoFocus true ]
+          Input.OnChange (fun ev -> !!ev.target?value |> UpdatingName |> dispatch)
+          Input.DefaultValue model.Name
+        ]
       ]
     ]
+    Field.div [ Field.IsGrouped ]
+      [ Control.div [ ] [
+        Button.Input.submit
+          [ Button.Color IsPrimary
+            Button.OnClick (fun _ -> dispatch AcceptName)
+            Button.Props [ Value "Done asd" ] ] ] ]
   ]
-  yield Field.div [ Field.IsGrouped ]
-    [ Control.div [ ] [
-      Button.Input.submit
-        [ Button.Color IsPrimary
-          Button.OnClick (fun _ -> dispatch AcceptName)
-          Button.Props [ Value "Done asd" ] ] ] ] }
+
+let knownUi model dispatch =
+  [
+    Field.div [ ] [
+      Label.label [ Label.Size Size.IsLarge ] [ str ("Welcome " + model.Name) ]
+      Label.label [ ] [ str "Start adding to your Xmas List" ] ]
+    Field.div [ ] [
+      ul [ ]
+        (model.Items
+        |> List.map (fun i -> li [] [ str i.Description])) ]
+    Field.div [ Field.HasAddons ] [
+      Control.div [ Control.IsExpanded ] [
+        Input.text [
+          Input.Value model.Current.Description
+          Input.DefaultValue model.Current.Description
+          Input.OnChange (fun ev -> !!ev.target?value |> UpdatingCurrent |> dispatch)
+          Input.Placeholder "What do you want?"
+          Input.Props [ Props.AutoFocus true ] ]
+      ]
+      Control.div [ ] [
+        Button.button [
+          Button.OnClick (fun _ -> dispatch Add)
+        ] [ str "Add"]
+      ] ]
+  ]
 
 let root model dispatch =
+
+  let ui =
+    match model.IsAccepted with
+    | false -> unknownUi
+    | true -> knownUi
+  let ui = ui model dispatch
+
   div [ ] [
-    form [ ] [
-      if (not model.IsAccepted) then
-        yield! unknownUi model dispatch
-      else
-        yield Field.div [ ] [
-              Label.label [ Label.Size Size.IsLarge ] [ str ("Welcome " + model.Name) ]
-              Label.label [ ] [ str "Start adding to your Xmas List" ] ]
-        yield Field.div [ ] [
-          ul [ ]
-            (model.Items
-            |> List.map (fun i -> li [] [ str i.Description]))
-        ]
-        yield Field.div [ Field.HasAddons ] [
-              Control.div [ Control.IsExpanded ] [
-                Input.text [
-                  Input.Value model.Current.Description
-                  Input.DefaultValue model.Current.Description
-                  Input.OnChange (fun ev -> !!ev.target?value |> UpdatingCurrent |> dispatch)
-                  Input.Placeholder "What do you want?" ]
-              ]
-              Control.div [ ] [
-                Button.button [
-                  Button.OnClick (fun _ -> dispatch Add)
-                ] [ str "Add"]
-              ] ]
-    ] ]
+    form [ ] ui
+  ]
